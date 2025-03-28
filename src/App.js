35966b46a7697a314afc7a1741808b0a96436d48
Gabe Delisle <133575614+Pepper0330@users.noreply.github.com>
2025-03-28 16:51:49 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import Homepage from './components/Homepage.js';
 import Courses from './components/CoursePage.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm.js';
-import SignUp from './components/SignUp.js';
+import SignUp from './components/SignUp.js'
+import { AuthProvider } from './components/AuthMessage.js';
+
+export const linkTitles = React.createContext(null);
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
-      
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
